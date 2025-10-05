917ce0a8104e68022a6c8b57b71390ae794351d2
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-05 22:28:07 +0100

    Added a page event supplier

--- src/main/java/q/jv/kafka/handlers/PageEventHandler.java
+++ src/main/java/q/jv/kafka/handlers/PageEventHandler.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import q.jv.kafka.events.PageEvent;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Component
 public class PageEventHandler {
@@ -14,6 +17,17 @@ public class PageEventHandler {
             System.out.println("***************");
             System.out.println(input.toString());
             System.out.println("***************");
+        };
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()->{
+            return new PageEvent(
+                    Math.random()>0.5?"P1":"P2"
+                    ,Math.random()>0.5?"U1":"U2"
+                    ,new Date()
+                    ,5+new Random().nextInt(1000));
         };
     }
 }
