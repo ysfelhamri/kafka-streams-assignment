b7ece8fd1d1d26723812d4154c887a2f04cd2359
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-05 22:03:50 +0100

    Defined a publish method for the page event

--- src/main/java/q/jv/kafka/controllers/PageEventController.java
+++ src/main/java/q/jv/kafka/controllers/PageEventController.java
@@ -1,4 +1,27 @@
 package q.jv.kafka.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import q.jv.kafka.events.PageEvent;
+
+import java.util.Date;
+import java.util.Random;
+
+@RestController
 public class PageEventController {
+    @Autowired
+    private StreamBridge streamBridge;
+    @GetMapping("/publish")
+    public PageEvent publish(String name, String topic){
+        PageEvent event = new PageEvent(
+                name,
+                Math.random()>0.5?"U1":"U2",
+                new Date()
+                ,5+new Random().nextInt(1000)
+        );
+        streamBridge.send(topic, event);
+        return event;
+    }
 }
