ac0019a705d613a91c3504ec2995f390d3555b79
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-05 22:13:20 +0100

    Defined a page event consumer

--- src/main/java/q/jv/kafka/handlers/PageEventHandler.java
+++ src/main/java/q/jv/kafka/handlers/PageEventHandler.java
@@ -1,4 +1,19 @@
 package q.jv.kafka.handlers;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import q.jv.kafka.events.PageEvent;
+
+import java.util.function.Consumer;
+
+@Component
 public class PageEventHandler {
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer(){
+        return (input)->{
+            System.out.printf("***************");
+            System.out.printf(input.toString());
+            System.out.printf("***************");
+        };
+    }
 }
