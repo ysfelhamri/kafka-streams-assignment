cc0a8fa47811ce81a2c866b00fb396665821a1ec
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-05 22:40:48 +0100

    Added a page event stream function

--- src/main/java/q/jv/kafka/handlers/PageEventHandler.java
+++ src/main/java/q/jv/kafka/handlers/PageEventHandler.java
@@ -1,5 +1,7 @@
 package q.jv.kafka.handlers;
 
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import q.jv.kafka.events.PageEvent;
@@ -7,6 +9,7 @@ import q.jv.kafka.events.PageEvent;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -29,5 +32,13 @@ public class PageEventHandler {
                     ,new Date()
                     ,5+new Random().nextInt(1000));
         };
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>,KStream<String, Long>> kStream(){
+        return (input)->
+            input.filter((k,v)->v.duration()>100)
+            .map((k,v)->new KeyValue<>(v.name(),v.duration()));
+
     }
 }
