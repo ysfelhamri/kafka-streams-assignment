c3c32b35cebc3a3ff88a907321777ef7dff6284a
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-07 13:15:37 +0100

    Added a windowing interval for the page event stream function

--- src/main/java/q/jv/kafka/handlers/PageEventHandler.java
+++ src/main/java/q/jv/kafka/handlers/PageEventHandler.java
@@ -4,10 +4,12 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import q.jv.kafka.events.PageEvent;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -40,10 +42,12 @@ public class PageEventHandler {
     public Function<KStream<String, PageEvent>,KStream<String, Long>> kStream(){
         return (input)->
             input.filter((k,v)->v.duration()>100)
-                .map((k,v)->new KeyValue<>(v.name(),v.duration()))
+                    .map((k,v)->new KeyValue<>(v.name(),v.duration()))
                     .groupByKey(Grouped.with(Serdes.String(),Serdes.Long()))
+                    .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
                     .count()
-                    .toStream();
+                    .toStream()
+                    .map((k,v)->new KeyValue<>(k.key(),v));
 
     }
 }
