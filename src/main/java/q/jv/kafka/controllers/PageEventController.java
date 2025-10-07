bf50a8e5d49c8c1ce10b9db5db90db8731b79f4d
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-07 13:21:48 +0100

    Added a flux function to the page event controller to pass the stream of data from the stream function to the front end

--- src/main/java/q/jv/kafka/controllers/PageEventController.java
+++ src/main/java/q/jv/kafka/controllers/PageEventController.java
@@ -1,18 +1,33 @@
 package q.jv.kafka.controllers;
 
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyWindowStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import q.jv.kafka.events.PageEvent;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @RestController
 public class PageEventController {
     @Autowired
     private StreamBridge streamBridge;
+    @Autowired
+    private InteractiveQueryService interactiveQueryService;
+
     @GetMapping("/publish")
     public PageEvent publish(String name, String topic){
         PageEvent event = new PageEvent(
@@ -23,5 +38,22 @@ public class PageEventController {
         );
         streamBridge.send(topic, event);
         return event;
+    }
+
+    @GetMapping(path = "/analytics",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Map<String, Long>> analytics(){
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence->{
+                    Map<String,Long> stringLongMap=new HashMap<>();
+                    ReadOnlyWindowStore<String, Long> windowStore = interactiveQueryService.getQueryableStore("count-store", QueryableStoreTypes.windowStore());
+                    Instant now=Instant.now();
+                    Instant from=now.minusMillis(5000);
+                    KeyValueIterator<Windowed<String>, Long> fetchAll = windowStore.fetchAll(from, now);
+                    while (fetchAll.hasNext()){
+                        KeyValue<Windowed<String>, Long> next = fetchAll.next();
+                        stringLongMap.put(next.key.key(),next.value);
+                    }
+                    return stringLongMap;
+                });
     }
 }
--- src/main/java/q/jv/kafka/handlers/PageEventHandler.java
+++ src/main/java/q/jv/kafka/handlers/PageEventHandler.java
@@ -4,6 +4,7 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -45,7 +46,7 @@ public class PageEventHandler {
                     .map((k,v)->new KeyValue<>(v.name(),v.duration()))
                     .groupByKey(Grouped.with(Serdes.String(),Serdes.Long()))
                     .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
-                    .count()
+                    .count(Materialized.as("count-store"))
                     .toStream()
                     .map((k,v)->new KeyValue<>(k.key(),v));
 
